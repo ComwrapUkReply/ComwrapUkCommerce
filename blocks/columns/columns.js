ef227c4e27b87c01617fce6accaa86e6c5fc8e44
s.sznajder@reply.com <s.sznajder@reply.com>
2024-05-08 16:44:18 +0100

    modify structure of the columns with two images

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,16 +1,26 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
-
-  // setup image columns
+  // Setup image columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
       if (pic) {
         const picWrapper = pic.closest('div');
-        if (picWrapper && picWrapper.children.length === 1) {
-          // picture is only content in column
+        if (picWrapper) {
+          // Picture is only content in column
           picWrapper.classList.add('columns-img-col');
+          // Now replace the div.columns-img-col with a ul while keeping the class
+          const ul = document.createElement('ul');
+          ul.classList.add('columns-img-col'); // Transfer the class to the ul
+          [...picWrapper.children].forEach((child) => {
+            const li = document.createElement('li');
+            li.appendChild(child.cloneNode(true)); // Clone and append to li
+            ul.appendChild(li); // Append li to ul
+          });
+
+          // Replace div with ul
+          picWrapper.parentNode.replaceChild(ul, picWrapper);
         }
       }
     });
