b20c1d528dbf9a89884de7b4867087d9028d97b0
Hannah <h.scott@reply.com>
2024-05-10 11:18:08 +0100

    fix hero errors

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 /* Hero core block */
 export default function decorate(block) {
   [...block.children].forEach((heroItem) => {
@@ -22,47 +23,4 @@ export default function decorate(block) {
       imageParent.replaceWith(newDiv);
     }
   });
-
-  // Select the h1 element with the class 'hero-title'
-  const heroTitle = document.querySelector(".hero-title");
-
-  // Create a new span element with the class 'span-title'
-  const titleSpan = document.createElement("span");
-  titleSpan.classList.add("span-title");
-
-  if (block === pageHome && block === animation) {
-    // Check if the h1 element contains text nodes
-    if (heroTitle.childNodes.length > 0) {
-      // Iterate over the child nodes of the h1 element
-      heroTitle.childNodes.forEach((node) => {
-        // Check if the node is a text node and not empty or whitespace
-        if (
-          node.nodeType === Node.TEXT_NODE &&
-          node.textContent.trim() !== ""
-        ) {
-          // Wrap the text content of the node in the new span element
-          node.replaceWith(titleSpan);
-          titleSpan.textContent = node.textContent;
-          const textContentArray = titleSpan.innerText.split(" ");
-          if (window.innerWidth <= 900) {
-            titleSpan.innerHTML = `<br>${textContentArray[0]} ${textContentArray
-              .slice(1)
-              .join(" ")}`;
-          } else {
-            titleSpan.innerHTML = ` ${
-              textContentArray[1]
-            } <br>${textContentArray.slice(2).join(" ")}`;
-          }
-        }
-      });
-    }
-  }
-
-  // Initializing generator
-  const gen = generator();
-
-  // Start the animation
-  if (block === pageHome && block === animation) {
-    printChar(words[gen.next().value]);
-  }
 }
