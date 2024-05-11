60eff4ddc4df173e157bfba02b6d7acaf772a19b
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-05-11 16:18:31 +0100

    Updated ESLint and Stylelint configurations
    Refined ESLint rules for better development practices, aligning with `eslint:recommended` and removing restrictions on development conveniences such as console usage and parameter reassignment. Adapted parser options for ES2021 support and streamlined codebase by allowing certain coding styles.
    
    Extended `.gitignore` and `.hlxignore` to exclude additional temporary files and directories like `scrap/` and `.vscode/`, ensuring a cleaner repository.
    
    Overhauled Stylelint settings to relax restrictions on styling rules, allowing for less stringent longhand property usage and more leeway in selector specificity, acknowledging varied coding styles and preferences.
    
    These changes aim to create a more developer-friendly environment and accommodate diverse coding practices while maintaining code quality.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,49 @@
 module.exports = {
-  root: true,
-  extends: 'airbnb-base',
   env: {
     browser: true,
+    es2021: true,
   },
-  parser: '@babel/eslint-parser',
+  extends: [
+    'eslint:recommended',
+    'airbnb-base',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
   parserOptions: {
-    allowImportExportEverywhere: true,
+    ecmaVersion: 'latest',
     sourceType: 'module',
-    requireConfigFile: false,
   },
   rules: {
-    // allow reassigning param
-    'no-param-reassign': [2, { props: false }],
-    'linebreak-style': ['error', 'unix'],
-    'import/extensions': ['error', {
-      js: 'always',
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'no-console': 'off',
+    'no-param-reassign': 'off',
+    'operator-linebreak': 'off',
+    'space-before-function-paren': 'off',
+    'no-plusplus': 'off',
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'never',
+      exports: 'always-multiline',
+      functions: 'never',
+    }],
+    'object-curly-newline': ['error', {
+      ObjectExpression: { multiline: true, consistent: true },
+      ObjectPattern: { multiline: true, consistent: true },
+      ImportDeclaration: { multiline: true, consistent: true },
+      ExportDeclaration: { multiline: true, consistent: true },
     }],
   },
 };
