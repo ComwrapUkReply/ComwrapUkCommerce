8a47d67593a1743fd152fe3032ece5f6ba5de5e9
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-05-11 16:50:33 +0100

    Refined ESLint config to align with Airbnb
    Streamlined the ESLint configuration to enforce Airbnb's base style guide as the primary extension, removing unnecessary plugins, globals, and overrides for simplicity. The parser is now set to '@babel/eslint-parser' with relaxed constraints on import/export usage and no requirement for a config file, offering greater flexibility. Adjusted rules to permit parameter reassignment for properties and mandated Unix-style line endings, enhancing consistency across different environments. Removed several previously disabled rules to embrace stricter code quality checks as per the Airbnb standard.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,58 +1,21 @@
 module.exports = {
+  root: true,
+  extends: 'airbnb-base',
   env: {
     browser: true,
-    es2021: true,
-    node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'airbnb-base',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-  ],
-  plugins: [
-    'import',
-  ],
-  globals: {
-    Intl: 'readonly',
-  },
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
-  ],
+  parser: '@babel/eslint-parser',
   parserOptions: {
-    ecmaVersion: 'latest',
+    allowImportExportEverywhere: true,
     sourceType: 'module',
+    requireConfigFile: false,
   },
   rules: {
-    'import/prefer-default-export': 'off',
-    'import/extensions': 'off',
-    'no-console': 'error',
-    'no-param-reassign': 'off',
-    'operator-linebreak': 'off',
-    'space-before-function-paren': 'off',
-    'no-plusplus': 'off',
-    'comma-dangle': ['error', {
-      arrays: 'always-multiline',
-      objects: 'always-multiline',
-      imports: 'always-multiline',
-      exports: 'always-multiline',
-      functions: 'never',
-    }],
-    'object-curly-newline': ['error', {
-      ObjectExpression: { multiline: true, consistent: true },
-      ObjectPattern: { multiline: true, consistent: true },
-      ImportDeclaration: { multiline: true, consistent: true },
-      ExportDeclaration: { multiline: true, consistent: true },
+    // allow reassigning param
+    'no-param-reassign': [2, { props: false }],
+    'linebreak-style': ['error', 'unix'],
+    'import/extensions': ['error', {
+      js: 'always',
     }],
   },
 };
