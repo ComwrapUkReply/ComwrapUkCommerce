c1ffeac77d4145ed90eeff6d5a37cf9c4bb5a781
s.sznajder@reply.com <s.sznajder@reply.com>
2024-05-16 12:15:18 +0100

    add only products

--- blocks/dynamic/dynamic.js
+++ blocks/dynamic/dynamic.js
@@ -23,7 +23,7 @@ export default async function decorate(block) {
   }
   const content = await ffetch('/query-index.json').all();
 
-  let targetNames = ['products,blog']; // Initialize targetNames with 'blog' as the default
+  let targetNames = ['products']; // Initialize targetNames with 'blog' as the default
 
   if (!window.location.pathname.endsWith('/')) {
   // Extract path segments excluding the domain
